a3b91137dc93d9c53f471ceb68802d5ab34dbf01
Melvin Shieh <shieh.melvin@gmail.com>
2025-09-01 17:05:49 -0400

    final todo version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from functools import wraps
+import os
 
 from flask import (
     flash,
@@ -187,4 +188,7 @@ def delete_todo(lst, todo, list_id, todo_id):
     return redirect(url_for('show_list', list_id=list_id))
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    if os.environ.get('FLASK_ENV') == 'production':
+        app.run(debug=False)
+    else:
+        app.run(debug=True, port=5003)
